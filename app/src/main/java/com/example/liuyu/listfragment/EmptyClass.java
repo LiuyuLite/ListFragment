a37aa1502f81e8085c6f9559b1e1cf9937cce052
Oliver <bratmliu@foxmail.com>
2016-03-21 12:38:51 +0800

    edit emptyclass

--- app/src/main/java/com/example/liuyu/listfragment/EmptyClass.java
+++ app/src/main/java/com/example/liuyu/listfragment/EmptyClass.java
@@ -4,4 +4,19 @@ package com.example.liuyu.listfragment;
  * Created by Liuyu on 2016/3/21.
  */
 public class EmptyClass {
+    // an empty just for git test
+    private String mName;
+    private int mSize;
+    public void setName(String name){
+        mName = name;
+    }
+    public String getName(){
+        return mName;
+    }
+    public void setSize(int size){
+        mSize = size;
+    }
+    public int getSize(){
+        return mSize;
+    }
 }
